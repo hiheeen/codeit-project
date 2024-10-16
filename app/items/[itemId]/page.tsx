e21b2bb11f9452c668d0a31519033d8fa63ff074
hiheeen <gmldms0338@naver.com>
2024-10-17 01:06:26 +0900

    Type 오류 수정 : 페이지 내의 함수 export 제거_2

--- app/items/[itemId]/page.tsx
+++ app/items/[itemId]/page.tsx
@@ -1,7 +1,7 @@
 import { BASE_URL } from '@/app/constants';
 import TodoDetail from '@/components/todo/TodoDetail';
 
-export async function getTodoDetail(itemId: string) {
+async function getTodoDetail(itemId: string) {
   const response = await fetch(`${BASE_URL}/items/${itemId}`, {
     cache: 'no-store', // 매번 최신 데이터를 가져오기 위해 사용
   });
